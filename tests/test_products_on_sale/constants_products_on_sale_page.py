a625712dcdc9d57ae4dec6b72a8b79de00b7c6a2
Natalya Mastyaeva <nata.m.754@gmail.com>
2023-08-09 19:14:31 +0400

    add: test_positive_check_discount_for_unauthorized_customer_for_amount_10000_14999rub_smoke ref: test_positive_check_discount_for_unauthorized_customer_for_amount_0_49rub_smoke

---
+++ tests/test_products_on_sale/constants_products_on_sale_page.py
@@ -0,0 +1,7 @@
+
+SHURUPY_PO_DEREVU_PAGE_URL = "catalog/shurupy-po-derevu"
+SHTUKATURNO_OTDELOCHNYJ_INSTRUMENT_PAGE_URL = "catalog/shtukaturno-otdelochnyj-instrument"
+
+USER_NAME = 'Тест'
+USER_PHONE = 9999999999
+USER_EMAIL = 'teststroy1@mail.ru'
