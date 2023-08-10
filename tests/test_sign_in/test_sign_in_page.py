c535e12e920cb570debe46c53ba6000f94a23372
Stas Cherepanov <94304561+zaelcovsky@users.noreply.github.com>
2023-08-11 00:02:46 +0700

    feat: Fix stroyrem_ci.yml

--- tests/test_sign_in/test_sign_in_page.py
+++ tests/test_sign_in/test_sign_in_page.py
@@ -5,6 +5,7 @@ from data.credentials import credentials
 from tests.test_sign_in.conftest import get_name_and_email_in_account
 from tests.test_sign_in.constants_sign_in_page import *
 from time import sleep
+from selenium.common import NoSuchElementException, NoSuchWindowException
 
 
 @allure.epic("Sign In Page")
@@ -42,7 +43,6 @@ class TestSignInPage:
             f" восстановления пароля не соответствует ожидаемому, получили {restore_password_page_heading_text}"
 
     @allure.title("Авторизация с помощью аккаунта mail.ru")
-    # @pytest.mark.xfail(reason="хранение логинов/паролей не реализовано")
     @pytest.mark.smoke_test
     def test_positive_authorization_first_time_using_MAIL_smoke(self, driver, sign_in_page, open_sign_in_window):
         sign_in_page.get_mail_auth_icon().click()
@@ -51,18 +51,15 @@ class TestSignInPage:
         sign_in_page.get_mail_ru_window_password_field().send_keys(credentials['mail.ru_password'])
         sign_in_page.get_mail_ru_window_submit_button().click()
         driver.switch_to.window(driver.window_handles[0])
-        # sign_in_page.check_number_of_windows_to_be_equal(1)
+        sign_in_page.check_number_of_windows_to_be_equal(1)
         sleep(10)
         assert driver.current_url == ACCOUNT_PAGE, f"Неправильный url страницы: {driver.current_url}"
-        driver.get(PERSONAL_DATA_PAGE)
-        sign_in_page.get_account_page_fizik_section().click()
-        name = sign_in_page.get_account_page_name_field().get_attribute('value')
-        email = sign_in_page.get_account_page_email_field().get_attribute('value')
+        name, email = get_name_and_email_in_account(driver, sign_in_page)
         assert name == credentials['mail.ru_name'] and email == credentials['mail.ru_email'], \
             f"ФИО или email не соответствуют ожидаемым, ФИО: {name}, email: {email}"
 
     @allure.title("Авторизация с помощью аккаунта OK.ru")
-    @pytest.mark.xfail(reason="хранение логинов/паролей не реализовано")
+    # @pytest.mark.xfail(reason="хранение логинов/паролей не реализовано")
     @pytest.mark.smoke_test
     def test_positive_authorization_first_time_using_OK_smoke(self, driver, sign_in_page, open_sign_in_window):
         sign_in_page.get_ok_ru_auth_icon().click()
@@ -72,22 +69,18 @@ class TestSignInPage:
         sign_in_page.get_ok_ru_window_submit_button().click()
         # на новых аккаунтах нужно еще кликнуть акцепт
         # self.driver.find_element(*self._ok_ru_window_accept_button).click()
-        driver.switch_to.window(driver.window_handles[0])
-        # sign_in_page.check_number_of_windows_to_be_equal(1)
+        # driver.switch_to.window(driver.window_handles[0])
+        sign_in_page.check_number_of_windows_to_be_equal(1)
         # c OK.ru не подтягивается email, нужно вводить вручную https://trello.com/c/D1M0KnkP
         # self.driver.find_element(*self._ulogin_window_email_field).send_keys(email)
         # self.driver.find_element(*self._ulogin_window_submit_button).click()
         sleep(10)
         assert driver.current_url == ACCOUNT_PAGE, f"Неправильный url страницы: {driver.current_url}"
-        driver.get(PERSONAL_DATA_PAGE)
-        sign_in_page.get_account_page_fizik_section().click()
-        name = sign_in_page.get_account_page_name_field().get_attribute('value')
-        email = sign_in_page.get_account_page_email_field().get_attribute('value')
+        name, email = get_name_and_email_in_account(driver, sign_in_page)
         assert name == credentials['ok.ru_name'] and email == credentials['ok.ru_email'], \
             f"ФИО или email не соответствуют ожидаемым, ФИО: {name}, email: {email}"
 
     @allure.title("Авторизация с помощью аккаунта vk.ru")
-    # @pytest.mark.xfail(reason="хранение логинов/паролей не реализовано")
     @pytest.mark.smoke_test
     def test_positive_authorization_first_time_using_VK_smoke(self, driver, sign_in_page, open_sign_in_window):
         sign_in_page.get_vk_ru_auth_icon().click()
@@ -98,50 +91,48 @@ class TestSignInPage:
         sign_in_page.get_vk_ru_window_submit_button().click()
         sign_in_page.get_vk_ru_window_continue_as_button().click()
         driver.switch_to.window(driver.window_handles[0])
-        # sign_in_page.check_number_of_windows_to_be_equal(1)
+        sign_in_page.check_number_of_windows_to_be_equal(1)
         sleep(10)
         assert driver.current_url == ACCOUNT_PAGE, f"Неправильный url страницы: {driver.current_url}"
-        driver.get(PERSONAL_DATA_PAGE)
-        sign_in_page.get_account_page_fizik_section().click()
-        name = sign_in_page.get_account_page_name_field().get_attribute('value')
-        email = sign_in_page.get_account_page_email_field().get_attribute('value')
+        name, email = get_name_and_email_in_account(driver, sign_in_page)
         assert name == credentials['vk.ru_name'] and email == credentials['vk.ru_email'], \
             f"ФИО или email не соответствуют ожидаемым, ФИО: {name}, email: {email}"
 
     @allure.title("Авторизация с помощью аккаунта ya.ru")
-    @pytest.mark.xfail(reason="хранение логинов/паролей не реализовано")
+    @pytest.mark.xfail
     @pytest.mark.smoke_test
     def test_positive_authorization_first_time_using_YA_smoke(self, driver, sign_in_page, open_sign_in_window):
         sign_in_page.get_ya_ru_auth_icon().click()
         driver.switch_to.window(driver.window_handles[1])
-        sign_in_page.get_ya_ru_window_alternative_ways_to_sign_up().click()
+        driver.execute_script("arguments[0].scrollIntoView();",
+                              sign_in_page.get_ya_ru_window_alternative_ways_to_sign_up())
+        driver.execute_script("arguments[0].click();", sign_in_page.get_ya_ru_window_alternative_ways_to_sign_up())
         sign_in_page.get_ya_ru_window_email_field().send_keys(credentials['ya.ru_email'])
         sign_in_page.get_ya_ru_window_submit_button().click()
         sign_in_page.get_ya_ru_window_password_field().send_keys(credentials['ya.ru_password'])
         sign_in_page.get_ya_ru_window_submit_button().click()
+        try:
+            sign_in_page.get_ya_ru_window_control_question_field()
+            sign_in_page.get_ya_ru_window_control_question_field().send_keys(credentials['ya.ru_question'])
+            sign_in_page.get_ya_ru_window_proceed_button().click()
+            print(f"\nПоявилось окно контрольного вопроса")
+        except (NoSuchWindowException, NoSuchElementException):
+            print(f"\nНе появилось окно контрольного вопроса")
         driver.switch_to.window(driver.window_handles[0])
-        # sign_in_page.check_number_of_windows_to_be_equal(1)
+        sign_in_page.check_number_of_windows_to_be_equal(1)
         sleep(10)
         assert driver.current_url == ACCOUNT_PAGE, f"Неправильный url страницы: {driver.current_url}"
-        driver.get(PERSONAL_DATA_PAGE)
-        sign_in_page.get_account_page_fizik_section().click()
-        name = sign_in_page.get_account_page_name_field().get_attribute('value')
-        email = sign_in_page.get_account_page_email_field().get_attribute('value')
+        name, email = get_name_and_email_in_account(driver, sign_in_page)
         assert name == credentials['ya.ru_name'] and email == credentials['ya.ru_email'], \
             f"ФИО или email не соответствуют ожидаемым, ФИО: {name}, email: {email}"
 
     @allure.title("Авторизация с помощью формы авторизации")
-    # @pytest.mark.xfail(reason="хранение логинов/паролей не реализовано")
     @pytest.mark.smoke_test
     def test_positive_login_with_correct_email_and_password_smoke(self, driver, sign_in_page, open_sign_in_window):
         sign_in_page.get_email_field().send_keys(credentials['email'])
         sign_in_page.get_password_field().send_keys(credentials['password'])
         sign_in_page.get_sign_in_button().click()
         assert driver.current_url == ACCOUNT_PAGE, f"Неправильный url страницы: {driver.current_url}"
-        # driver.get(PERSONAL_DATA_PAGE)
-        # sign_in_page.get_account_page_fizik_section().click()
-        # name = sign_in_page.get_account_page_name_field().get_attribute('value')
-        # email = sign_in_page.get_account_page_email_field().get_attribute('value')
         name, email = get_name_and_email_in_account(driver, sign_in_page)
         assert name == credentials['name'] and email == credentials['email'], \
             f"ФИО или email не соответствуют ожидаемым, ФИО: {name}, email: {email}"
