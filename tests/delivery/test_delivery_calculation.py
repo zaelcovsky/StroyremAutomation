c0ac78ace51dba416acdad01eaaba44672e02b26
Karbolin Ivan <karboliniv@gmail.com>
2023-08-30 22:59:45 +0300

    Delivery | calculation blue zone

--- tests/delivery/test_delivery_calculation.py
+++ tests/delivery/test_delivery_calculation.py
@@ -72,7 +72,72 @@ class TestDeliveryPageCalculation:
         ordering_page.fill_city(city)
         ordering_page.fill_street(address_green_zone)
         ordering_page.select_delivery('standard')
-        time.sleep(3)
+        time.sleep(2)
+
+        standard_delivery_price = ordering_page.get_delivery_price('standard')
+        standard_delivery_value = int(''.join(filter(str.isdigit, standard_delivery_price)))
+        assert standard_delivery_value == standard, f'Стоимость стандартной доставки не соответствует расчетной:' \
+                                                    f'\nТекущая цена: {standard_delivery_value}' \
+                                                    f'\nОжидаемая цена: {standard}'
+
+        delivery_in_time_price = ordering_page.select_delivery('in_time').get_delivery_price('in_time')
+        delivery_in_time_value = int(''.join(filter(str.isdigit, delivery_in_time_price)))
+        assert delivery_in_time_value == in_time, f'Стоимость доставки ко времени не соответствует расчетной:' \
+                                                  f'\nТекущая цена: {delivery_in_time_value}' \
+                                                  f'\nОжидаемая цена: {in_time}'
+
+        delivery_all_time_price = ordering_page.select_delivery('all_time').get_delivery_price('all_time')
+        delivery_all_time_value = int(''.join(filter(str.isdigit, delivery_all_time_price)))
+        assert delivery_all_time_value == all_time, f'Стоимость круглосуточной доставки не соответствует расчетной:' \
+                                                    f'\nТекущая цена: {delivery_all_time_value}' \
+                                                    f'\nОжидаемая цена: {all_time}'
+
+    @pytest.mark.parametrize('case_id, link_item, amount, weight, standard, in_time, all_time', [
+        ("TD 015", bundeks_25, 1, 30, 650, 2390, 3320),
+        pytest.param(
+            "TD 016", aksolit_30, 8, 300, 650, 2390, 3320,
+            marks=pytest.mark.xfail(reson="Цена доставки: - - -")
+        ),
+        ("TD 017", aksolit_30, 13, 400, 650, 2390, 3320),
+        ("TD 018", aksolit_30, 16, 500, 850, 2490, 3320),
+        ("TD 019", aksolit_30, 25, 1700, 1150, 2490, 3320),
+        ("TD 020", aksolit_30, 66, 2000, 1500, 2990, 3650),
+        ("TD 021", aksolit_30, 149, 4500, 2600, 4380, 4890),
+        ("TD 022", aksolit_30, 166, 5000, 2800, 5490, 5980),
+        pytest.param(
+            "TD 023", aksolit_30, 206, 6200, 3300, 7500, 8000,
+            marks=pytest.mark.xfail(reson="Цена доставки: 4300 9900 11440")
+        ),
+        ("TD 024", aksolit_30, 299, 9000, 4300, 9900, 11440),
+        ("TD 025", aksolit_30, 333, 10000, 7000, 13900, 15900),
+        pytest.param(
+            "TD 026", aksolit_30, 366, 11000, 8000, 14900, 19900,
+            marks=pytest.mark.xfail(reson="Цена доставки: 7000 13900 15900")
+        ),
+        pytest.param(
+            "TD 027", aksolit_30, 666, 20000, 14000, 16900, 23900,
+            marks=pytest.mark.xfail(reson="Цена доставки: 7000 13900 15900")
+        )
+    ])
+    @pytest.mark.smoke
+    def test_positive_delivery_blue_zone_smoke(
+            self, driver, url, case_id, link_item, amount, weight, standard, in_time, all_time
+    ):
+        allure.dynamic.title(f"{case_id} Доставка товара весом до {weight}кг. в синей зоне")
+
+        driver.get(f'{url}{link_item}')
+        item_page = ItemPage(driver)
+        item_page.add_item_to_cart(amount).click_on_cart()
+
+        cart_page = BasketPage(driver)
+        cart_page.click_on_go_to_checkout()
+
+        ordering_page = OrderingPage(driver)
+        ordering_page.fill_fiz_name(fio).fill_fiz_phone(form_phone).fill_fiz_email(email).click_on_continue()
+        ordering_page.fill_city(city)
+        ordering_page.fill_street(address_blue_zone)
+        ordering_page.select_delivery('standard')
+        time.sleep(2)
 
         standard_delivery_price = ordering_page.get_delivery_price('standard')
         standard_delivery_value = int(''.join(filter(str.isdigit, standard_delivery_price)))
